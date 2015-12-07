a3e9998eea100dd10c6aea4c20216ebd4815558a
jleewebdev <lee45276@gmail.com>
2015-12-07 13:57:07 +0800

    Added more methods to TodoList class

--- todo.rb
+++ todo.rb
@@ -115,6 +115,30 @@ def <<(item)
     result
   end
 
+
+  def find_by_title(title)
+      select { |todo| todo.title == title}.first
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo| todo.undone! }
+  end
 end
 
 
@@ -129,5 +153,6 @@ list.add(todo1)
 list.add(todo2)
 list.add(todo3)
 
-p list.each {|todo| puts todo}
+p list.mark_done("Buy mil")
+p list
 
